ce6f747a5135b8ac87c4eef2677ddc980134573c
roxeannechico <roseannechico22@gmail.com>
2024-10-19 19:42:18 +0800

    initial commit

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Pickup truck",
+    model: "Tundra",
+    color: "Blueprint"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
